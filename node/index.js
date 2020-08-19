69c7aefb292effbfc01ea422097a0dcc4947192a
rixterx3 <69008132+rixterx3@users.noreply.github.com>
2020-08-19 21:10:56 +0545

    Update index.js

--- node/index.js
+++ node/index.js
@@ -13,7 +13,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
- * Awesome ci
+ * Awesome got
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
